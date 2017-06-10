f9a3923c03d27d16cd3bee93bb450d3091ea9696
Amit Yadav <amityadav@amityadav.in>
2017-06-10 17:04:20 +0530

    fix

--- accounts/views.py
+++ accounts/views.py
@@ -1,29 +1,27 @@
-# Django imports
-from accounts.forms import SignUpForm, SignInForm, PasswordForgotForm, \
-    PasswordResetForm, PasswordChangeForm,ResendActivationEmailForm
-from django.contrib.auth.decorators import login_required
-from accounts.models import PasswordForgot, RegistrationProfile
+import datetime
+
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import make_password, check_password
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render_to_response, render, get_object_or_404
-from django.template import RequestContext
-from oposod.settings import  RANDOM_CHARS, SITE
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from redis import StrictRedis
 
+from accounts.forms import SignUpForm, SignInForm, PasswordForgotForm, \
+    PasswordResetForm, PasswordChangeForm, ResendActivationEmailForm
+from accounts.models import PasswordForgot, RegistrationProfile
+from oposod.settings import RANDOM_CHARS, SITE
 from users.models import Profile, PrivacySettings, ProfilePhoto
 from utils.misc import send_html_mail, activation_key_generator
-from django.contrib import messages
 
-# Python imports
-import datetime
-from redis import StrictRedis
 
 def signup(request):
     form = SignUpForm(request.POST or None)
     if form.is_valid():
-        
+
         first_name = form.cleaned_data['first_name']
         last_name = form.cleaned_data['last_name']
         username = form.cleaned_data['username']
@@ -31,43 +29,43 @@ def signup(request):
         password = form.cleaned_data['password']
 
         user_obj = User.objects.create(
-            first_name = first_name.title(), 
-            last_name = last_name.title(),
-            username = username,
-            email = email,
-            date_joined = datetime.datetime.now(),
+            first_name=first_name.title(),
+            last_name=last_name.title(),
+            username=username,
+            email=email,
+            date_joined=datetime.datetime.now(),
             is_active=False,
-            password = make_password(password),
+            password=make_password(password),
             is_superuser=False,
             is_staff=False,
-            )
-           
-        
+        )
+
         profile = Profile(user=user_obj,
-                        dob = form.cleaned_data['dob'],
-                        sex = form.cleaned_data['sex'],
-                        last_edited_on = datetime.datetime.now(),
-            )
+                          dob=form.cleaned_data['dob'],
+                          sex=form.cleaned_data['sex'],
+                          last_edited_on=datetime.datetime.now(),
+                          )
         profile.save()
 
         PrivacySettings.objects.create(user=user_obj,
-            friends_visibility = 'F',
-            cover_photos_visibility = 'F',
-            profile_photos_visibility = 'F',
-            daily_photos_visibility = 'F',
-            stories_visibility = 'F',
-            calendar_visibility = 'F',
-            )
+                                       friends_visibility='F',
+                                       cover_photos_visibility='F',
+                                       profile_photos_visibility='F',
+                                       daily_photos_visibility='F',
+                                       stories_visibility='F',
+                                       calendar_visibility='F',
+                                       )
         ProfilePhoto.objects.create(user=user_obj,
-            profile_photo='profile_photo/default_avatar.jpg',
-            cropping = '',
-            is_set=True,
-            key = '123456789',
-            uploaded_on=datetime.datetime.now())
+                                    profile_photo='profile_photo/default_avatar.jpg',
+                                    cropping='',
+                                    is_set=True,
+                                    key='123456789',
+                                    uploaded_on=datetime.datetime.now())
 
-        #Sending the confirmation email here.
+        # Sending the confirmation email here.
         try:
-            ACTIVATION_KEY = activation_key_generator(20, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdegfhijklmnopqrstuvwxyz1234567890")
+            ACTIVATION_KEY = activation_key_generator(20,
+                                                      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdegfhijklmnopqrstuvwxyz1234567890")
             subject = 'Oposod.com: User %s creation information.' % (username)
             html_content = '''<div style="border: 1px solid #ddd; background: #000; padding: 30px; font-size: 50px; font-family: 'Ubuntu'">
                             <p>Hello %s . You have successfully registered your account on oposod.com
@@ -80,19 +78,20 @@ def signup(request):
 
             rp = RegistrationProfile(user=user_obj, activation_key=ACTIVATION_KEY)
             rp.save()
-            
+
         except Exception, e:
             return render(request, '404_error.html', {
                 'msg': 'There is some error in sending the email. The error is: %s' % e
             })
 
-        
-        messages.info(request, 'You have successfully registered, please activate your account from your email before signing in.')
+        messages.info(request,
+                      'You have successfully registered, please activate your account from your email before signing in.')
         return HttpResponseRedirect(reverse('accounts.views.signin'))
 
-    return render_to_response('accounts/signup.html', {
-            'form': form,
-        }, context_instance = RequestContext(request))
+    return render(request, 'accounts/signup.html', {
+        'form': form,
+    })
+
 
 def signin(request):
     if request.user.is_authenticated():
@@ -103,13 +102,13 @@ def signin(request):
         password = form.cleaned_data['password']
         user_obj = None
         try:
-            user_obj = User.objects.get(username = username_or_email)
+            user_obj = User.objects.get(username=username_or_email)
         except:
-            user_obj = User.objects.get(email = username_or_email)
+            user_obj = User.objects.get(email=username_or_email)
 
         if user_obj:
             if check_password(password, user_obj.password):
-                user = authenticate(username = user_obj.username, password = password)
+                user = authenticate(username=user_obj.username, password=password)
                 if user:
                     if user.is_active:
                         login(request, user)
@@ -123,14 +122,14 @@ def signin(request):
                         if next:
                             return HttpResponseRedirect(next)
                         else:
-                            return HttpResponseRedirect(reverse('users.views.profile', args=(str(user_obj.username),)))
+                            return HttpResponseRedirect(reverse('profile', args=(str(user_obj.username),)))
 
                 else:
                     messages.info(request, 'Please activate your account first.')
                     return HttpResponseRedirect(reverse('accounts.views.signin.'))
-    return render_to_response('accounts/signin.html', {
-            'form': form,
-        }, context_instance = RequestContext(request))
+    return render(request, 'accounts/signin.html', {
+        'form': form,
+    })
 
 
 def reset_password_ask_email_page(request):
@@ -148,7 +147,7 @@ def reset_password_ask_email_page(request):
         if user_obj:
             token = activation_key_generator(6, RANDOM_CHARS)
             print 'token: ', token
-            
+
             pf_obj = PasswordForgot.objects.filter(user=user_obj)
             if pf_obj:
                 pf_obj.delete()
@@ -168,12 +167,13 @@ def reset_password_ask_email_page(request):
             except Exception, e:
                 print 'error in sending email: %s' % e
 
-            messages.info(request, 'An email with the token has been send to the given email with a token. Enter that token here to proceed further')
+            messages.info(request,
+                          'An email with the token has been send to the given email with a token. Enter that token here to proceed further')
             return HttpResponseRedirect(reverse('accounts.views.reset_password_do_reset_password_page'))
         else:
             print 'no user'
     else:
-        print 'some error in form'        
+        print 'some error in form'
 
     return render(request, 'accounts/reset_password_ask_email_page.html', {
         'form': form,
@@ -185,7 +185,7 @@ def reset_password_do_reset_password_page(request):
     if form.is_valid():
         token = form.cleaned_data['token']
         pass1 = form.cleaned_data['pass1']
-        #pass2 = form.cleaned_data['pass2']
+        # pass2 = form.cleaned_data['pass2']
 
         password_forgot_obj = get_object_or_404(PasswordForgot, token=token)
         User.objects.filter(id=password_forgot_obj.user_id).update(password=make_password(pass1))
@@ -199,7 +199,6 @@ def reset_password_do_reset_password_page(request):
 
 @login_required
 def change_password(request):
-
     form = PasswordChangeForm(request.user, request.POST or None)
     if form.is_valid():
         new_password = form.cleaned_data['new_password']
@@ -207,10 +206,11 @@ def change_password(request):
         messages.info(request, 'Your password has been change sucessfully.')
         return HttpResponseRedirect(reverse('accounts.views.change_password'))
 
-    return render_to_response(
-        'accounts/change_password.html', {
-            'form': form,
-        }, context_instance=RequestContext(request))
+    return render(request,
+                  'accounts/change_password.html', {
+                      'form': form,
+                  }, )
+
 
 def signout(request):
     # Remove user from the set online:users
@@ -223,7 +223,6 @@ def signout(request):
 
 
 def activate(request, activation_key):
-
     '''
         If the user has clicked the email link send to his email, then it is considered
         that the email id is used while registartion is valid and hence his profile
@@ -250,15 +249,13 @@ def activate(request, activation_key):
     else:
         return HttpResponseRedirect('/404')
 
-    return HttpResponse('')
-
 
 def resend_activation_email(request):
     form = ResendActivationEmailForm(request.POST or None)
     if form.is_valid():
         email = form.cleaned_data['email']
-        user_obj = get_object_or_404(User, email = email)
-        rp_obj = get_object_or_404(RegistrationProfile, user = user_obj)
+        user_obj = get_object_or_404(User, email=email)
+        rp_obj = get_object_or_404(RegistrationProfile, user=user_obj)
         subject = 'Resending email activation on request'
         html_content = '''<div style="border: 1px solid #ddd; background: #000; padding: 30px; font-size: 50px; font-family: 'Ubuntu'">
                         <p>Hello, </p>
