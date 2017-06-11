97bda9cc557105bf32e4511dca4cb32f615222a6
Amit Yadav <amityadav@amityadav.in>
2017-06-11 10:42:23 +0530

    fix

--- utils/privacy_decorators.py
+++ utils/privacy_decorators.py
@@ -1,23 +1,21 @@
-
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
-from users.models import PrivacySettings, Friends
 
+from users.models import PrivacySettings, Friends
 
 
 def visibility_of_daily_photos(function):
     def wrapper(request, *args, **kwargs):
-        user_from_url = get_object_or_404(User, username = kwargs['username'])
-        
-        if request.user.is_authenticated(): 
+        user_from_url = get_object_or_404(User, username=kwargs['username'])
+
+        if request.user.is_authenticated():
             logged_in_user = request.user
         else:
             logged_in_user = None
-            
-    
-        ps_obj_for_user_from_url = get_object_or_404(PrivacySettings, user = user_from_url)
-        
+
+        ps_obj_for_user_from_url = get_object_or_404(PrivacySettings, user=user_from_url)
+
         if ps_obj_for_user_from_url.daily_photos_visibility == 'N':
             if not logged_in_user:
                 return HttpResponseRedirect('/403')
@@ -26,42 +24,39 @@ def visibility_of_daily_photos(function):
                     return function(request, *args, **kwargs)
                 else:
                     return HttpResponseRedirect('/403')
-        
+
         if ps_obj_for_user_from_url.daily_photos_visibility == 'A':
             return function(request, *args, **kwargs)
-        
+
         if ps_obj_for_user_from_url.daily_photos_visibility == 'F':
-            
+
             if not logged_in_user:
                 return HttpResponseRedirect('/403')
             else:
                 if logged_in_user.username == user_from_url.username:
                     return function(request, *args, **kwargs)
                 else:
-                    
-                    if int(logged_in_user.id) in get_object_or_404(Friends, user = user_from_url).list_of:
+
+                    if int(logged_in_user.id) in get_object_or_404(Friends, user=user_from_url).list_of:
                         return function(request, *args, **kwargs)
                     else:
                         return HttpResponseRedirect('/403')
-            
+
     return wrapper
-
-
 
 
 def visibility_of_friends(function):
     def wrapper(request, *args, **kwargs):
 
-        user_from_url = get_object_or_404(User, username = kwargs['username'])
-        
-        if request.user.is_authenticated(): 
+        user_from_url = get_object_or_404(User, username=kwargs['username'])
+
+        if request.user.is_authenticated():
             logged_in_user = request.user
         else:
             logged_in_user = None
-            
-    
-        ps_obj_for_user_from_url = get_object_or_404(PrivacySettings, user = user_from_url)
-        
+
+        ps_obj_for_user_from_url = get_object_or_404(PrivacySettings, user=user_from_url)
+
         if ps_obj_for_user_from_url.friends_visibility == 'N':
             if not logged_in_user:
                 return HttpResponseRedirect('/403')
@@ -70,10 +65,10 @@ def visibility_of_friends(function):
                     return function(request, *args, **kwargs)
                 else:
                     return HttpResponseRedirect('/403')
-        
+
         if ps_obj_for_user_from_url.friends_visibility == 'A':
             return function(request, *args, **kwargs)
-        
+
         if ps_obj_for_user_from_url.friends_visibility == 'F':
             if not logged_in_user:
                 return HttpResponseRedirect('/403')
@@ -81,27 +76,27 @@ def visibility_of_friends(function):
                 if logged_in_user.username == user_from_url.username:
                     return function(request, *args, **kwargs)
                 else:
-                    
-                    if int(logged_in_user.id) in get_object_or_404(Friends, user = user_from_url).list_of:
+
+                    if int(logged_in_user.id) in get_object_or_404(Friends, user=user_from_url).list_of:
                         return function(request, *args, **kwargs)
                     else:
                         return HttpResponseRedirect('/403')
-            
+
     return wrapper
+
 
 def visibility_of_stories(function):
     def wrapper(request, *args, **kwargs):
-     
-        user_from_url = get_object_or_404(User, username = kwargs['username'])
-        
-        if request.user.is_authenticated(): 
+
+        user_from_url = get_object_or_404(User, username=kwargs['username'])
+
+        if request.user.is_authenticated():
             logged_in_user = request.user
         else:
             logged_in_user = None
-            
-    
-        ps_obj_for_user_from_url = get_object_or_404(PrivacySettings, user = user_from_url)
-        
+
+        ps_obj_for_user_from_url = get_object_or_404(PrivacySettings, user=user_from_url)
+
         if ps_obj_for_user_from_url.stories_visibility == 'N':
             if not logged_in_user:
                 return HttpResponseRedirect('/403')
@@ -110,10 +105,10 @@ def visibility_of_stories(function):
                     return function(request, *args, **kwargs)
                 else:
                     return HttpResponseRedirect('/403')
-        
+
         if ps_obj_for_user_from_url.stories_visibility == 'A':
             return function(request, *args, **kwargs)
-        
+
         if ps_obj_for_user_from_url.stories_visibility == 'F':
             if not logged_in_user:
                 return HttpResponseRedirect('/403')
@@ -121,28 +116,27 @@ def visibility_of_stories(function):
                 if logged_in_user.username == user_from_url.username:
                     return function(request, *args, **kwargs)
                 else:
-                    
-                    if int(logged_in_user.id) in get_object_or_404(Friends, user = user_from_url).list_of:
+
+                    if int(logged_in_user.id) in get_object_or_404(Friends, user=user_from_url).list_of:
                         return function(request, *args, **kwargs)
                     else:
                         return HttpResponseRedirect('/403')
-            
+
     return wrapper
 
 
 def visibility_of_calendar(function):
     def wrapper(request, *args, **kwargs):
-        
-        user_from_url = get_object_or_404(User, username = kwargs['username'])
-        
-        if request.user.is_authenticated(): 
+
+        user_from_url = get_object_or_404(User, username=kwargs['username'])
+
+        if request.user.is_authenticated():
             logged_in_user = request.user
         else:
             logged_in_user = None
-            
-    
-        ps_obj_for_user_from_url = get_object_or_404(PrivacySettings, user = user_from_url)
-        
+
+        ps_obj_for_user_from_url = get_object_or_404(PrivacySettings, user=user_from_url)
+
         if ps_obj_for_user_from_url.calendar_visibility == 'N':
             if not logged_in_user:
                 return HttpResponseRedirect('/403')
@@ -151,10 +145,10 @@ def visibility_of_calendar(function):
                     return function(request, *args, **kwargs)
                 else:
                     return HttpResponseRedirect('/403')
-        
+
         if ps_obj_for_user_from_url.calendar_visibility == 'A':
             return function(request, *args, **kwargs)
-        
+
         if ps_obj_for_user_from_url.calendar_visibility == 'F':
             if not logged_in_user:
                 return HttpResponseRedirect('/403')
@@ -162,29 +156,27 @@ def visibility_of_calendar(function):
                 if logged_in_user.username == user_from_url.username:
                     return function(request, *args, **kwargs)
                 else:
-                    
-                    if int(logged_in_user.id) in get_object_or_404(Friends, user = user_from_url).list_of:
+
+                    if int(logged_in_user.id) in get_object_or_404(Friends, user=user_from_url).list_of:
                         return function(request, *args, **kwargs)
                     else:
                         return HttpResponseRedirect('/403')
-            
-    return wrapper
 
+    return wrapper
 
 
 def visibility_of_cover_photos(function):
     def wrapper(request, *args, **kwargs):
-       
-        user_from_url = get_object_or_404(User, username = kwargs['username'])
-        
-        if request.user.is_authenticated(): 
+
+        user_from_url = get_object_or_404(User, username=kwargs['username'])
+
+        if request.user.is_authenticated():
             logged_in_user = request.user
         else:
             logged_in_user = None
-            
-    
-        ps_obj_for_user_from_url = get_object_or_404(PrivacySettings, user = user_from_url)
-        
+
+        ps_obj_for_user_from_url = get_object_or_404(PrivacySettings, user=user_from_url)
+
         if ps_obj_for_user_from_url.cover_photos_visibility == 'N':
             if not logged_in_user:
                 return HttpResponseRedirect('/403')
@@ -193,10 +185,10 @@ def visibility_of_cover_photos(function):
                     return function(request, *args, **kwargs)
                 else:
                     return HttpResponseRedirect('/403')
-        
+
         if ps_obj_for_user_from_url.cover_photos_visibility == 'A':
             return function(request, *args, **kwargs)
-        
+
         if ps_obj_for_user_from_url.cover_photos_visibility == 'F':
             if not logged_in_user:
                 return HttpResponseRedirect('/403')
@@ -204,28 +196,27 @@ def visibility_of_cover_photos(function):
                 if logged_in_user.username == user_from_url.username:
                     return function(request, *args, **kwargs)
                 else:
-                    
-                    if int(logged_in_user.id) in get_object_or_404(Friends, user = user_from_url).list_of:
+
+                    if int(logged_in_user.id) in get_object_or_404(Friends, user=user_from_url).list_of:
                         return function(request, *args, **kwargs)
                     else:
                         return HttpResponseRedirect('/403')
-            
+
     return wrapper
 
 
 def visibility_of_profile_photos(function):
     def wrapper(request, *args, **kwargs):
-        
-        user_from_url = get_object_or_404(User, username = kwargs['username'])
-        
-        if request.user.is_authenticated(): 
+
+        user_from_url = get_object_or_404(User, username=kwargs['username'])
+
+        if request.user.is_authenticated():
             logged_in_user = request.user
         else:
             logged_in_user = None
-            
-    
-        ps_obj_for_user_from_url = get_object_or_404(PrivacySettings, user = user_from_url)
-        
+
+        ps_obj_for_user_from_url = get_object_or_404(PrivacySettings, user=user_from_url)
+
         if ps_obj_for_user_from_url.profile_photos_visibility == 'N':
             if not logged_in_user:
                 return HttpResponseRedirect('/403')
@@ -234,10 +225,10 @@ def visibility_of_profile_photos(function):
                     return function(request, *args, **kwargs)
                 else:
                     return HttpResponseRedirect('/403')
-        
+
         if ps_obj_for_user_from_url.profile_photos_visibility == 'A':
             return function(request, *args, **kwargs)
-        
+
         if ps_obj_for_user_from_url.profile_photos_visibility == 'F':
             if not logged_in_user:
                 return HttpResponseRedirect('/403')
@@ -245,10 +236,10 @@ def visibility_of_profile_photos(function):
                 if logged_in_user.username == user_from_url.username:
                     return function(request, *args, **kwargs)
                 else:
-                    
-                    if int(logged_in_user.id) in get_object_or_404(Friends, user = user_from_url).list_of:
+
+                    if int(logged_in_user.id) in get_object_or_404(Friends, user=user_from_url).list_of:
                         return function(request, *args, **kwargs)
                     else:
                         return HttpResponseRedirect('/403')
-            
+
     return wrapper
