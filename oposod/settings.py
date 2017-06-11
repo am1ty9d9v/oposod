6b769b8bae58e0a9a488cbda9d077d5e5934d32a
Amit Yadav <amityadav@amityadav.in>
2017-06-11 12:19:33 +0530

    Added dev and prod settings

--- oposod/settings.py
+++ oposod/settings.py
@@ -229,33 +229,10 @@ AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
     'Cache-Control': 'max-age=94608000',
 }
 
-AWS_HOST_NAME = 's3.ap-south-1.amazonaws.com'
-AWS_STORAGE_BUCKET_NAME = 'amityadav'
-AWS_ACCESS_KEY_ID = 'AKIAIC6J4U55M66CM4CQ'
-AWS_SECRET_ACCESS_KEY = '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'
 
-# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
-# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
-# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
-# We also use it in the next setting.
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
-# refers directly to STATIC_URL. So it's safest to always set it.
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
-
-# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
-# you run `collectstatic`).
-MEDIAFILES_LOCATION = 'media'
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-
-
-if os.getenv("MODE", "PROD"):
+if os.getenv("MODE") == "PROD":
     from prod_settings import *
-elif os.getenv("MODE", "TEST"):
+elif os.getenv("MODE") == "TEST":
     from test_settings import *
 else:
     from dev_settings import *
